57dc1c9c477a7cf4cdfcdafe6613b1015c2bea14
Fearless4Him <107826307+Fearless4Him@users.noreply.github.com>
2022-09-10 00:06:40 -0400

    Commit initial files

---
+++ index.js
@@ -0,0 +1,41 @@
+let homeScoreEl = document.getElementById("home-score")
+let homeScoreBtnOne = document.getElementById("home-score-btn-1")
+let homeScoreBtnTwo = document.getElementById("home-score-btn-2")
+let homeScoreBtnThree = document.getElementById("home-score-btn-3")
+let homeScore = 0
+
+function increaseHomeScoreOne() {
+    homeScore += 1
+    homeScoreEl.textContent = homeScore
+}
+
+function increaseHomeScoreTwo() {
+    homeScore += 2
+    homeScoreEl.textContent = homeScore
+}
+
+function increaseHomeScoreThree() {
+    homeScore += 3
+    homeScoreEl.textContent = homeScore
+}
+
+let guestScoreEl = document.getElementById("away-score")
+let guestScoreBtnOne = document.getElementById("guest-score-btn-1")
+let guestScoreBtnTwo = document.getElementById("guest-score-btn-2")
+let guestScoreBtnThree = document.getElementById("guest-score-btn-3")
+let guestScore = 0
+
+function increaseGuestScoreOne() {
+    guestScore += 1
+    guestScoreEl.textContent = guestScore
+}
+
+function increaseGuestScoreTwo() {
+    guestScore += 2
+    guestScoreEl.textContent = guestScore
+}
+
+function increaseGuestScoreThree() {
+    guestScore += 3
+    guestScoreEl.textContent = guestScore
+}
